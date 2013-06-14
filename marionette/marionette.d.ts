bdebb684a3f7dc0347cc128148cd774273bd86a2
Zeeshan Hamid <zee@budzee.com>
2013-06-14 10:49:15 -0400

    Updating marionette.d.ts with more definitions.

--- marionette/marionette.d.ts
+++ marionette/marionette.d.ts
@@ -5,73 +5,159 @@
 
 /// <reference path="../backbone/backbone.d.ts" />
 
-declare module Marionette {
+declare module Backbone {
 
-  export class View extends Backbone.View {
-    constructor(options?: any);
+  export module Wreqr {
 
-    modelEvents: any;
-    collectionEvents: any;
-    ui: any;
+    export class Handlers extends Backbone.Model {
 
-    getTemplate(): any;
-    mixinTemplateHelpers(target?: any): any;
-    configureTriggers(): any;
-    delegateEvents(events?: any): any;
-    undelegateEvents();
+      constructor(options?);
 
-    close();
-    bindUIElements();
-    unbindUIElements();
+      public setHandlers(options?);
+      public setHandler(name: string, handler: any, context?: any);
+
+      public hasHandler(name: string): Boolean;
+      public getHandler(name: string): any;
+
+      public removeHandler(name: string);
+      public removeAllHandlers();
+    }
+
+    export class CommandStorage extends Backbone.Events {
+      constructor(options?);
+
+      getCommands(commandName: string);
+      addCommand(commandName: string, arg: any);
+      clearCommands(commandName: string);
+
+    }
+
+    export class Commands extends Backbone.Wreqr.Handlers {
+      constructor(options?);
+
+      execute(name: string, arg: any);
+    }
+
+    export class EventAggregator {
+    }
+
+    export class RequestResponse extends Backbone.Wreqr.Handlers {
+    }
   }
 
-  export class ItemView extends Marionette.View {
-    ui: any;
+  export module Marionette {
+    export class View extends Backbone.View {
+      constructor(options?: any);
 
-    constructor(options?: any);
+      modelEvents: any;
+      collectionEvents: any;
+      ui: any;
 
-    serializeData(): any;
-    render(): View;
-    close();
+      getTemplate(): any;
+      mixinTemplateHelpers(target?: any): any;
+      configureTriggers(): any;
+      delegateEvents(events?: any): any;
+      undelegateEvents();
+
+      close();
+      bindUIElements();
+      unbindUIElements();
+    }
+
+    export class ItemView extends Marionette.View {
+      ui: any;
+
+      constructor(options?: any);
+
+      serializeData(): any;
+      render(): View;
+      close();
+    }
+
+    export class CollectionView extends Marionette.View {
+      itemView: any;
+      emptyView: any;
+
+      constructor(options?: any);
+
+      _initialEvents();
+      addChildView(item: View, collection: View, options?: any);
+      onShowCalled();
+
+      triggerBeforeRender();
+      triggerRendered();
+      render(): View;
+      renderModel(): View;
+
+      getItemView(item: any): ItemView;
+      addItemView(item: any, ItemView: ItemView, index: Number);
+      renderItemView(view: View, index: Number);
+      buildItemView(item: any, ItemViewType: any, itemViewOptions: any): any;
+      removeItemView(item: any);
+      removeChildView(view: View);
+
+      checkEmpty();
+
+      appendHtml(collectionView: View, itemView: View, index: Number);
+
+      close();
+      closeChildren();
+      addChildViewEventForwarding(view: View);
+    }
+
+
+    export class CompositeView extends Marionette.CollectionView {
+      itemView: any;
+      itemViewContainer: string;
+
+      constructor(options?: any);
+
+      appendHtml(cv: any, iv: any, index: Number);
+      getItemViewContainer(iv: any);
+    }
+
+    export class Controller {
+      constructor(options?);
+    }
+
+
+
+    export class AppRouter extends Backbone.Router {
+
+      controller: any;
+      appRoutes: any;
+
+
+      constructor(options?);
+
+      processAppRoutes(controller: Controller, appRoutes: AppRouter);
+
+    }
+
+    class Application extends Backbone.Events {
+      addInitializer(callback);
+      addRegion(region);
+      removeRegion(region);
+
+      commands: Backbone.Wreqr.Commands;
+      vent: Backbone.Wreqr.EventAggregator;
+      reqres: Backbone.Wreqr.RequestResponse;
+
+      start();
+
+      module(moduleNames, moduleDefinition);
+      execute(name: string, args?: any);
+    }
+
+    class Module extends Backbone.Events {
+
+      create(app: any, moduleNames: any, moduleDefinition: any);
+
+      addInitializer(callback: any);
+      addFinalizer(callback: any);
+      start(options: any);
+      stop();
+      addDefinition(moduleDefinition: any, customArgs: any);
+    }
   }
-
-  export class CollectionView extends Marionette.View {
-    itemView: any;
-
-    constructor(options?: any);
-
-    _initialEvents();
-    addChildView(item: View, collection: View, options?: any);
-    onShowCalled();
-
-    triggerBeforeRender();
-    triggerRendered();
-    render(): View;
-
-    getItemView(item: any): ItemView;
-    addItemView(item: any, ItemView: ItemView, index: Number);
-    renderItemView(view: View, index: Number);
-    buildItemView(item: any, ItemViewType: any, itemViewOptions: any): any;
-    removeItemView(item: any);
-    removeChildView(view: View);
-
-    checkEmpty();
-
-    appendHtml(collectionView: View, itemView: View, index: Number);
-
-    close();
-    closeChildren();
-    addChildViewEventForwarding(view: View);
-  }
-
-
-  export class CompositeView extends Marionette.CollectionView {
-    itemView: any;
-    itemViewContainer: string;
-
-    constructor(options?: any);
-
-    appendHtml(cv: any, iv: any);
-  }
-
-}
+};
